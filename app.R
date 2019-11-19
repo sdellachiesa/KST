95a8faefb9784971feab7597c4ce266fcd14dc6b
Stefano Della Chiesa <stefano.dellachiesa@eurac.edu>
2019-11-19 08:40:47 +0100

    added rsconnect to deploy the app to shiny.io

--- app.R
+++ app.R
@@ -4,35 +4,58 @@ if (!require("leaflet.providers")) install.packages("leaflet.providers")
 if (!require("magrittr")) install.packages("magrittr")
 if (!require("RColorBrewer")) install.packages("RColorBrewer")
 if (!require("dplyr")) install.packages("dplyr")
+if (!require("rsconnect")) install.packages("connect")
 
 # --- Load Data
 
-mymap<-read.csv("KSTS_1_2ord_CSV.csv", stringsAsFactors=F)
+mydata<-read.csv("KSTS_1_2ord_CSV.csv", stringsAsFactors=F)
+mydata$ord1<-mydata$Ord == "First"
+mydata$ord2<-mydata$Ord == "Second"
 
 
-# Define UI for application that shows a map
+
 ui <- fluidPage(
-     # Main panel for Output
-  mainPanel(
-    # Output: map
-    leafletOutput("mymap"),
-    p()
-  )
+    sliderInput(inputId = "slider", 
+                label = "values",
+                min = min(df$Elevation),
+                max = max(df$Elevation),
+                value = 500,
+                step = 100),
+    leafletOutput("my_leaf")
 )
 
-server <- function(input, output) {
-  
-  points <- eventReactive(input$recalc, {
-    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-  }, ignoreNULL = FALSE)
-  
-  output$mymap <- renderLeaflet({
-    leaflet() %>%
-      addProviderTiles(providers$Stamen.TonerLite,
-                       options = providerTileOptions(noWrap = TRUE)
-      ) %>%
-      addMarkers(~lat,~lon,)
-  })
+server <- function(input, output, session){
+    df <- mydata
+    ## create static element
+    output$my_leaf <- renderLeaflet({
+        leaflet() %>%
+            addProviderTiles(providers$OpenTopoMap, group='Topo') %>%
+            addProviderTiles(providers$Esri.WorldImagery, group='Satellite') %>%
+            addLayersControl(baseGroups = c('Topo', 'Satellite'))%>%
+            setView(13.169629, 50.860422, zoom = 7)
+       
+        
+    })
+    
+    ## filter data
+    df_filtered <- reactive({
+        df[df$Elevation >= input$slider, ]
+    })
+    
+    ## respond to the filtered data
+    observe({
+        
+        leafletProxy(mapId = "my_leaf", data = df_filtered()) %>%
+            clearMarkers() %>%   ## clear previous markers
+            addMarkers()
+    })
+    
 }
 
 shinyApp(ui, server)
+
+# ----
+library(rsconnect)
+getwd()
+rsconnect::deployApp("~/00_Stefano/21_R/app.R")
+
