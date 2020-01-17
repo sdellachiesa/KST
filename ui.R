784cecf8954130c4bd372c50f5c2842da0205424
Stefano Della Chiesa <stefano.dellachiesa@eurac.edu>
2020-01-17 08:27:56 +0100

    testing fork me in github badge

--- ui.R
+++ ui.R
@@ -1,5 +1,8 @@
-ui <- fluidPage(
-  h1("Königlich-Sächsische Triangulirung"),
+ui <- fluidPage( 
+  #img(src="./www/KST_Logo.png", align = "right",height='50px',width='100px'),
+  #titlePanel(title=div(img(src="./www/KST_Logo.jpg",align = "right",height = 50, width = 100))),
+  #<a href="https://github.com/sdellachiesa"><img width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_green_007200.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"></a>
+  h1("Königlich-Sächsische Triangulierung"),
   p(style = "font-family:Impact",
     " ",
     a("Wikipedia",
@@ -7,6 +10,7 @@ ui <- fluidPage(
   ),
   sidebarLayout(sidebarPanel(
     leafletOutput("my_leaf"),width = 8),
+    
     mainPanel(sliderInput(inputId = "slider", 
                           label = "Wählen Sie den Höhenbereich [m]",
                           min = min(df$Hoehe),
