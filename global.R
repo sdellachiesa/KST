ab7160f26c2b36b3f14a42db371d6a0bc3a13c77
Stefano Della Chiesa <stefano.dellachiesa@eurac.edu>
2020-01-17 08:26:51 +0100

    testing markersize

--- global.R
+++ global.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinydashboard)
 library(leaflet)
 library(leaflet.providers)
 library(magrittr)
@@ -7,7 +8,6 @@ library(dplyr)
 library(rgdal)
 library(utf8)
 library(sf)
-
 # --- Load Data
 df<-read.csv('./data/KST_MERGED_1_2ord_CSV.csv')
 df$lat = as.numeric(format(df$lat, digits = 9))
@@ -15,13 +15,14 @@ df$lon = as.numeric(format(df$lon, digits = 9))
 # --- Data table to be showed in ui
 df_table<-df[c(3,7,8)]
 
-# --- shape file boundary
+# --- shape file boundary 
 my_shp1<-st_read('./data/FederalStateBoundary.shp')
 my_shp2<-st_read('./data/KST_1st_ord.shp')
 my_shp2$Length<-my_shp2$Length/1000
 my_shp2$Length = as.numeric(format(my_shp2$Length, digits = 2))
 # Create icons
 Green = makeAwesomeIcon(icon = "ios-close", iconColor = "white",
+                        extraClasses = list(width = '1000px'),
                         library = "ion", markerColor = "green")
 Red = makeAwesomeIcon(icon = "ios-close", iconColor = "white",
                       library = "ion", markerColor = "red")
