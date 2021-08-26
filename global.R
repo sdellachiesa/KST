056d720e9b720a2f3a1e868c7d0369ef237616e9
Stefano Della Chiesa <stefano.dellachiesa@gmail.com>
2021-08-26 11:49:21 +0200

    no changes, removed an empty line

--- global.R
+++ global.R
@@ -40,3 +40,4 @@ Red = makeAwesomeIcon(icon = "ios-close", iconColor = "white",
 df$IconsCol = ifelse(df$Ordnung  ==1,
                      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
                      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png")
+
